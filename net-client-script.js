0282e20c56bd1952b74ca39c5051e5c379dbe188
Thunwara <mtheewara@gmail.com>
2023-05-22 17:39:31 +0700

    first commit

---
+++ net-client-script.js
@@ -0,0 +1,58 @@
+function onChange(control, oldValue, newValue, isLoading) {
+  if (isLoading || newValue == "") {
+    return;
+  }
+  //Type appropriate comment here, and begin script below
+  if (
+    g_form.getValue("start_date_time") == "" ||
+    g_form.getValue("end_date_time") == "" ||
+    g_form.getValue("vehicle_registration_number") == ""
+  ) {
+    return;
+  }
+  //Get time and vehicle rid from portal form
+  var startDate = new Date(g_form.getValue("start_date_time")).getTime();
+  var endDate = new Date(g_form.getValue("end_date_time")).getTime();
+  var nowDate = new Date().getTime();
+  var regId = g_form.getValue("vehicle_registration_number");
+  //Check if startDate bf now or endDate bf startDate
+  if (nowDate > startDate || endDate < startDate || endDate < nowDate) {
+    g_form.addErrorMessage("Your start date or end date is not correct.");
+    g_form.clearValue("start_date_time");
+    g_form.clearValue("end_date_time");
+  } else {
+    var ga = new GlideAjax("carParkingTimeAjax"); //Run GlideAjax
+    ga.addParam("sysparm_name", "getTime");
+    ga.addParam("sysparm_car_regis_number", regId);
+    ga.addParam("sysparm_start_time", g_form.getValue("start_date_time"));
+    ga.addParam("sysparm_end_time", g_form.getValue("end_date_time"));
+    ga.getXML(timeCalculated);
+    function timeCalculated(response) {
+      var timeDiffMin =
+        response.responseXML.documentElement.getAttribute("answer");
+      if (timeDiffMin == 0) {
+        alert(
+          "Cannot booking.\nYour vehicle number " +
+            regId +
+            " has been booked.\nPlease select another time for a new booking.\nThank you"
+        );
+        g_form.clearValue("start_date_time");
+        g_form.clearValue("end_date_time");
+      } else {
+        var timeDiffHr = Math.ceil(timeDiffMin / 60);
+        var fee = 0;
+        //More than 8 HR
+        if (timeDiffHr > 8) {
+          fee = 200;
+        } else if (timeDiffHr > 4 && timeDiffHr <= 8) {
+          fee = 55 + (timeDiffHr - 4) * 20;
+        } else if (timeDiffHr > 1 && timeDiffHr <= 4) {
+          fee = 10 + (timeDiffHr - 1) * 15;
+        } else if (timeDiffHr > 0 && timeDiffHr <= 1) {
+          fee = 10;
+        }
+        g_form.setValue("parking_fee", fee);
+      }
+    }
+  }
+}
---
+++ net-script-include.js
@@ -0,0 +1,32 @@
+var carParkingTimeAjax = Class.create();
+carParkingTimeAjax.prototype =
+Object.extendsObject(AbstractAjaxProcessor, {
+    getTime: function(){
+
+    //Pass Parameter from Client-Script
+    var carRID = this.getParameter('sysparm_car_regis_number');
+    var startDateTime = this.getParameter('sysparm_start_time');
+    var endDateTime = this.getParameter('sysparm_end_time');
+    var startDateSplit = startDateTime.split(" ");
+    var endDateSplit = endDateTime.split(" ");
+
+    //Query Record
+    var carRecord = new GlideRecord('sc_req_item');
+
+    //Query SELECT carID is carRID
+    carRecord.addEncodedQuery("active=true^variables.a0c57d8a47262110b6bcf2e7536d4317="+carRID);
+
+    //Query SELECT Date time in Range of startTime and endTime
+    carRecord.addEncodedQuery("active=true^variables.a0c57d8a47262110b6bcf2e7536d4317="+carRID+"^variables.79d5bd8a47262110b6bcf2e7536d439a<javascript:gs.dateGenerate('" +startDateSplit[0]+ "','"+ startDateSplit[1] +"')^variables.20f531ca47262110b6bcf2e7536d4368>javascript:gs.dateGenerate('" + endDateSplit[0] + "','" + endDateSplit[1] + "')^NQactive=true^variables.a0c57d8a47262110b6bcf2e7536d4317="+carRID+"^variables.79d5bd8a47262110b6bcf2e7536d439aBETWEENjavascript:gs.dateGenerate('" +startDateSplit[0]+ "','"+ startDateSplit[1] +"')@javascript:gs.dateGenerate('" + endDateSplit[0] + "','" + endDateSplit[1] + "')^ORvariables.20f531ca47262110b6bcf2e7536d4368BETWEENjavascript:gs.dateGenerate('" +startDateSplit[0]+ "','"+ startDateSplit[1] +"')@javascript:gs.dateGenerate('" + endDateSplit[0] + "','" + endDateSplit[1] + "')");
+    carRecord.query();
+    
+    var count = 0;
+    while (carRecord.next()){
+        //Count number of record
+        //If count > 0 -> Found item on query option
+        count = count + 1;
+    }
+    return count;
+    },
+    type: 'carParkingTimeAjax'
+});
