64f892b92a2e312ece3a8206ee330e14fc825590
Thunwara <mtheewara@gmail.com>
2023-05-31 16:58:26 +0700

    send more parameters back to script-client successfully

--- my-script-client.js
+++ my-script-client.js
@@ -1,6 +1,6 @@
 function onChange(control, oldValue, newValue, isLoading) {
   if (isLoading || newValue === "") {
-    return;
+      return;
   }
 
   // Get time and vehicle rid from portal form
@@ -11,34 +11,42 @@ function onChange(control, oldValue, newValue, isLoading) {
 
   // Check if startDate is before now or endDate is before startDate or endDate is before nowDate
   if (nowDate > startDate || endDate < startDate || endDate < nowDate) {
-    g_form.addErrorMessage("Your start date or end date is not correct.");
-    g_form.clearValue("start");
-    g_form.clearValue("end");
+      g_form.addErrorMessage("Your start date or end date is not correct.");
+      g_form.clearValue("start");
+      g_form.clearValue("end");
   } else {
-    // Create a GlideAjax object
-    var ga = new GlideAjax("ParkingFeeCalculator");
-    ga.addParam("sysparm_name", "getTime");
-    ga.addParam("sysparm_car_regis_number", regId);
-    ga.addParam("sysparm_start_time", g_form.getValue("start"));
-    ga.addParam("sysparm_end_time", g_form.getValue("end"));
+      // Create a GlideAjax object
+      var ga = new GlideAjax("ParkingFeeCalculator");
+      ga.addParam("sysparm_name", "getTime");
+      ga.addParam("sysparm_car_regis_number", regId);
+      ga.addParam("sysparm_start_time", g_form.getValue("start"));
+      ga.addParam("sysparm_end_time", g_form.getValue("end"));
 
-    // Make the asynchronous AJAX call
-    ga.getXML(timeThun);
+      // Make the asynchronous AJAX call
+      ga.getXML(timeThunCallback);
   }
 
-  function timeThun(response) {
-    var answer = response.responseXML.documentElement.getAttribute("answer");
-    alert(answer);
-    g_form.setValue("fee", answer);
+  function timeThunCallback(response) {
+      var jsonResponse = response.responseXML.documentElement.getAttribute("answer");
+      var result = JSON.parse(jsonResponse);
 
-    // Retrieve additional parameters passed from server script
-    var parameter1 =
-      response.responseXML.documentElement.getAttribute("parameter1");
-    var parameter2 =
-      response.responseXML.documentElement.getAttribute("parameter2");
+      var durationHours = result.durationHours;
+      var totalFee = result.totalFee;
+      g_form.setValue("duration", durationHours + " hour(s)");
+      g_form.setValue("fee", totalFee + " bath");
 
-    // Use the parameters as needed
-    console.log("Parameter 1: " + parameter1);
-    console.log("Parameter 2: " + parameter2);
+      // var xmlDoc = response.responseXML;
+      // var durationMinutes = xmlDoc.documentElement.getAttribute("durationMinutes");
+      // var durationHours = xmlDoc.documentElement.getAttribute("durationHours");
+      // alert(durationHours + " " + durationMinutes);
+
+      // var result = JSON.parse(response);
+      // var param1 = result.param1;
+      // var param2 = result.param2;
+      // alert("param are " + param1 + param2);
+
+      // // Use the parameters as needed
+      // console.log("Parameter 1: " + parameter1);
+      // console.log("Parameter 2: " + parameter2);
   }
 }
--- my-script-include.js
+++ my-script-include.js
@@ -1,63 +1,86 @@
 var ParkingFeeCalculator = Class.create();
 ParkingFeeCalculator.prototype = Object.extendsObject(AbstractAjaxProcessor, {
-  getTime: function () {
-    // Pass Parameter from Client-Script
-    var carRID = this.getParameter("sysparm_car_regis_number");
-    var startDateTimeString = this.getParameter("sysparm_start_time");
-    var endDateTimeString = this.getParameter("sysparm_end_time");
+    getTime: function() {
+        // Pass Parameter from Client-Script
+        var carRID = this.getParameter("sysparm_car_regis_number");
+        var startDateTimeString = this.getParameter("sysparm_start_time");
+        var endDateTimeString = this.getParameter("sysparm_end_time");
 
-    // Convert startDateTimeString and endDateTimeString to GlideDateTime objects
-    var startDateTime = new GlideDateTime();
-    startDateTime.setDisplayValue(startDateTimeString);
+        // Convert startDateTimeString and endDateTimeString to GlideDateTime objects
+        var startDateTime = new GlideDateTime();
+        startDateTime.setDisplayValue(startDateTimeString);
 
-    var endDateTime = new GlideDateTime();
-    endDateTime.setDisplayValue(endDateTimeString);
+        var endDateTime = new GlideDateTime();
+        endDateTime.setDisplayValue(endDateTimeString);
 
-    // Query Record
-    var carRecord = new GlideRecord("sc_req_item");
+        // Query Record
+        var carRecord = new GlideRecord("sc_req_item");
 
-    // Query SELECT carID is carRID
-    carRecord.addEncodedQuery(
-      "active=true^cat_item.nameSTARTSWITHCar Park Reservations" + carRID
-    );
-    carRecord.query();
+        // Query SELECT carID is carRID
+        carRecord.addEncodedQuery(
+            "active=true^cat_item.nameSTARTSWITHCar Park Reservations" + carRID
+        );
+        carRecord.query();
 
-    // Calculate duration in minutes
-    var duration = GlideDateTime.subtract(startDateTime, endDateTime);
-    var durationInMinutes = Math.ceil(duration.getNumericValue() / (1000 * 60)); // Convert duration to minutes
+        // Calculate duration in minutes
+        var duration = GlideDateTime.subtract(startDateTime, endDateTime);
+        var durationInMinutes = Math.ceil(duration.getNumericValue() / (1000 * 60)); // Convert duration to minutes
 
-    gs.info("Duration in minutes: " + durationInMinutes);
+        gs.info("Duration in minutes: " + durationInMinutes);
 
-    // Convert duration to hours and round up
-    var durationInHours = Math.ceil(durationInMinutes / 60); // Convert duration to hours and round up
+        // Convert duration to hours and round up
+        var durationInHours = Math.ceil(durationInMinutes / 60); // Convert duration to hours and round up
 
-    gs.info("Duration in hours: " + durationInHours);
+        gs.info("Duration in hours: " + durationInHours);
 
-    // Calculate fee
-    var fee;
-    if (durationInHours < 60) {
-      fee = durationInHours * 10;
-    } else if (durationInHours < 240) {
-      fee = durationInHours * 15;
-    } else if (durationInHours < 480) {
-      fee = durationInHours * 15;
-    } else {
-      fee = 200;
-    }
+        // Calculate fee
+        var fee;
+        if (durationInHours < 60) {
+            fee = durationInHours * 10;
+        } else if (durationInHours < 240) {
+            fee = durationInHours * 15;
+        } else if (durationInHours < 480) {
+            fee = durationInHours * 15;
+        } else {
+            fee = 200;
+        }
 
-    // Create a JSON object to hold the parameters
-    var response = {
-      durationMinutes: durationInMinutes,
-      durationHours: durationInHours,
-    };
+        // Create a JSON object to hold the parameters
+        var response = {
+            durationMinutes: durationInMinutes,
+            durationHours: durationInHours,
+            totalFee: fee,
+        };
 
-    // Convert the JSON object to a string
-    var jsonResponse = JSON.stringify(response);
+        // Convert the JSON object to a string
+        var jsonResponse = JSON.stringify(response);
 
-    // Set the response type and send the JSON string back to the client script
-    this.setResponseType(this.JSON);
-    return jsonResponse;
-  },
+        // Set the response type and send the JSON string back to the client script
+        this.setResponseType(this.JSON);
+        return jsonResponse;
 
-  type: "ParkingFeeCalculator",
+        // var xmlDoc = new XMLDocument();
+        // var root = xmlDoc.createElement("response");
+        // xmlDoc.appendChild(root);
+
+        // var durationMinutesNode = xmlDoc.createElement("durationMinutes");
+        // durationMinutesNode.setAttribute("value", durationInMinutes);
+        // root.appendChild(durationMinutesNode);
+
+        // var durationHoursNode = xmlDoc.createElement("durationHours");
+        // durationHoursNode.setAttribute("value", durationInHours);
+        // root.appendChild(durationHoursNode);
+
+        // // Convert the XML document to a string
+        // var xmlString = xmlDoc.toXMLString();
+
+        // // Set the response type and send the XML string back to the client script
+        // this.setResponseType(this.XML);
+        // return xmlString;
+
+    },
+
+
+
+    type: "ParkingFeeCalculator",
 });
